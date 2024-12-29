98902a7dde0a3b2220fe8d79ce382159391073c6
Gabriel de Carvalho <biel.tirzrsl@gmail.com>
2024-12-29 16:44:35 -0300

    começando teste unitarios

---
+++ src/test/java/com/ead/authuser/services/impl/UserServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.ead.authuser.services.impl;
+
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repositories.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+    @InjectMocks
+    UserServiceImpl userServiceImpl;
+
+    @Mock
+    UserRepository userRepository;
+
+
+    @Test
+    void findAll() {
+        //SETUP: Prepara o ambiente de teste
+        UserModel userModel = new UserModel();
+        userModel.setUserId(UUID.randomUUID());
+        userModel.setUsername("username");
+
+        List<UserModel> userModelList = new ArrayList<>();
+        userModelList.add(userModel);
+
+        Mockito.when(userRepository.findAll()).thenReturn(userModelList);
+
+        // EXECUÇÃO: chama o metodo que esta sendo testatdo (findAll() no service)
+        List<UserModel> userModelList1 = userServiceImpl.findAll();
+
+
+        // VERIFICAÇÃO: Aqui verificamos se o resultado do metodo é o esperado
+        Assertions.assertEquals(userModelList, userModelList1);
+        Assertions.assertEquals(userModelList.size(), userModelList1.size());
+        Mockito.
+                verify(userRepository, Mockito.times(1)).findAll();
+        Mockito.
+                verifyNoMoreInteractions(userRepository);
+
+    }
+}
