690caeceb1da0585a8d512db05e6c50aebca1064
Gabriel de Carvalho <biel.tirzrsl@gmail.com>
2025-01-01 21:07:45 -0300

    criando validacoes customizadas PasswordConstraint

---
+++ src/main/java/com/ead/authuser/validations/PasswordConstraintImpl.java
@@ -0,0 +1,29 @@
+package com.ead.authuser.validations;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Pattern;
+
+public class PasswordConstraintImpl implements ConstraintValidator<PasswordConstraint, String> {
+
+    private static final String PASSWORD_PATTERN =   "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()-[{}]:;',?/*~$^+=<>]).{6,20}$";
+
+
+    @Override
+    public void initialize(PasswordConstraint constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        if (password == null || password.trim().isEmpty() || password.contains(" ")){
+            return false;
+            //verficar si essa String sastifaz as regras que criamos acima
+        }else if (!pattern.matcher(password).matches()){
+            return false;
+        }
+        return true;
+    }
+}
