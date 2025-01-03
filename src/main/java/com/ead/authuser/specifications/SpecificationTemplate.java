1aaf00680df2ccfe6aebe7934583131f4dabcb52
Gabriel de Carvalho <biel.tirzrsl@gmail.com>
2025-01-03 15:58:16 -0300

    implementado Filtros Avançados em APIs com Specification

---
+++ src/main/java/com/ead/authuser/specifications/SpecificationTemplate.java
@@ -0,0 +1,23 @@
+package com.ead.authuser.specifications;
+
+import com.ead.authuser.models.UserModel;
+import net.kaczmarzyk.spring.data.jpa.domain.Equal;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
+import org.springframework.data.jpa.domain.Specification;
+
+public class SpecificationTemplate {
+
+    @And({
+
+            @Spec(path = "userType", spec = Equal.class),
+            @Spec(path = "userStatus", spec = Equal.class),
+            @Spec(path = "email", spec = Equal.class),
+            @Spec(path = "userName", spec = Equal.class),
+            @Spec(path = "fullName", spec = Equal.class)
+    })
+
+
+
+    public interface UserSpec extends Specification<UserModel> {}
+}
