52de8353fbd746bef70fdbab1a089c9c9bafd37f
Gabriel de Carvalho <biel.tirzrsl@gmail.com>
2025-02-11 16:41:55 -0300

    Implementação e Boas Práticas de Logging nos Microservices com Log4J2

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -4,14 +4,20 @@ package com.ead.authuser.controllers;
 import com.ead.authuser.dtos.UserRecordDto;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
+
+    Logger logger = LogManager.getLogger(AuthenticationController.class);
 
     final UserService userService;
 
@@ -22,15 +28,26 @@ public class AuthenticationController {
     @PostMapping("signup")
     public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserRecordDto.UserView.RegistrationPost.class)
                                                    @JsonView(UserRecordDto.UserView.RegistrationPost.class) UserRecordDto userRecordDto){
+        logger.debug("POST registerUser userRecordDto received {}", userRecordDto);
         if (userService.existsByUsername(userRecordDto.username())){
+            logger.warn("Username {} is already exists", userRecordDto.username());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("esse Username ja existe");
         }
         if (userService.existsByEmail(userRecordDto.email())){
+            logger.warn("Username {} is already exists", userRecordDto.email());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("esse Email ja existe");
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(userRecordDto));
     }
 
-
+    @GetMapping("/logs")
+    public String index(){
+        logger.trace("TRACE");
+        logger.debug("DEBUG");
+        logger.info("INFO");
+        logger.warn("WARN");
+        logger.error("ERROR");
+        return "Logging Spring Boot";
+    }
 
 }
--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.UserRecordDto;
+import com.ead.authuser.exceptions.GlobalExceptionHandler;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.ead.authuser.specifications.SpecificationTemplate;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -22,6 +25,8 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequestMapping("/users")
 //@CrossOrigin(origins = "*", maxAge = 3600) //nivel de classe para ficar como exemplo
 public class UserController {
+
+    Logger logger = LogManager.getLogger(UserController.class);
 
     //ponto de injeção
     final UserService userService;
@@ -49,6 +54,7 @@ public class UserController {
 
     @DeleteMapping("{userId}")
     public ResponseEntity<Object> deleteUser(@PathVariable(value = "userId") UUID userId) {
+        logger.debug("DELETE deleteUser userId received {} ", userId);
         userService.delete(userService.findById(userId).get());
         return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully");
     }
@@ -58,6 +64,7 @@ public class UserController {
                                              @RequestBody @Validated(UserRecordDto.UserView.UserPut.class)
                                              @JsonView(UserRecordDto.UserView.UserPut.class)
                                              UserRecordDto userRecordDto) {
+        logger.debug(" PUT updateUser userRecordDto received {} ", userRecordDto);
         return ResponseEntity.status(HttpStatus.OK).body(userService.updateUser(userRecordDto, userService.findById(userId).get()));
     }
 
@@ -65,6 +72,7 @@ public class UserController {
     public ResponseEntity<Object> updatePassword(@PathVariable(value = "userId") UUID userId,
                                                  @RequestBody @Validated(UserRecordDto.UserView.PasswordPut.class)
                                                  @JsonView(UserRecordDto.UserView.PasswordPut.class) UserRecordDto userRecordDto) {
+        logger.debug(" PUT updatePassword userId received {} ", userId);
 
         //verificar se o ID existe na base de dados
         Optional<UserModel> userModelOptional = userService.findById(userId);
@@ -72,6 +80,7 @@ public class UserController {
         //a senha que o Usuario estiver passando tem que ser igual a senha antiga
 
         if (!userModelOptional.get().getPassword().equals(userRecordDto.oldPassword())){
+            logger.warn("Mismatched old password! userId {} ", userId);
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: senha nao corresponde a senha antiga");
         }
         userService.updatePassword(userRecordDto, userModelOptional.get());
@@ -82,6 +91,7 @@ public class UserController {
     public ResponseEntity<Object> updateImage(@PathVariable(value = "userId") UUID userId,
                                               @RequestBody @Validated(UserRecordDto.UserView.ImagePut.class)
                                               @JsonView(UserRecordDto.UserView.ImagePut.class) UserRecordDto userRecordDto) {
+        logger.debug("PUT updateImage userRecordDto received {} ", userRecordDto);
         return ResponseEntity.status(HttpStatus.OK).body(userService.updateImage(userRecordDto, userService.findById(userId).get()));
     }
 
