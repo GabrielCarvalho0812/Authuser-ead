2859ee91b5f509e300c9e49b0183bdece09c0ab6
Gabriel de Carvalho <biel.tirzrsl@gmail.com>
2025-03-31 14:32:45 -0300

    Preparação da Deleção de Recursos entre Microservices

--- src/main/java/com/ead/authuser/repositories/UserRepository.java
+++ src/main/java/com/ead/authuser/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import com.ead.authuser.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+
 import java.util.UUID;
 
 
@@ -12,4 +13,5 @@ public interface UserRepository  extends JpaRepository <UserModel, UUID>, JpaSpe
 
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
 }
