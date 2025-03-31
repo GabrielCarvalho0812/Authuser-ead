603e9e203de269649eb2ed11d965fe2b1818b6ea
Gabriel de Carvalho <biel.tirzrsl@gmail.com>
2025-03-31 13:40:05 -0300

    Validação Customizada - Custom Validation em User

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -3,12 +3,14 @@ package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.UserRecordDto;
 import com.ead.authuser.services.UserService;
+import com.ead.authuser.validations.UserValidator;
 import com.fasterxml.jackson.annotation.JsonView;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,25 +22,33 @@ public class AuthenticationController {
     Logger logger = LogManager.getLogger(AuthenticationController.class);
 
     final UserService userService;
+    final UserValidator userValidator;
 
-    public AuthenticationController(UserService userService){
+    public AuthenticationController(UserService userService, UserValidator userValidator){
         this.userService = userService;
+        this.userValidator = userValidator;
     }
 
     @PostMapping("signup")
     public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserRecordDto.UserView.RegistrationPost.class)
-                                                   @JsonView(UserRecordDto.UserView.RegistrationPost.class) UserRecordDto userRecordDto){
+                                                   @JsonView(UserRecordDto.UserView.RegistrationPost.class) UserRecordDto userRecordDto,
+                                               Errors errors) {
         logger.debug("POST registerUser userRecordDto received {}", userRecordDto);
-        if (userService.existsByUsername(userRecordDto.username())){
-            logger.warn("Username {} is already exists", userRecordDto.username());
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("esse Username ja existe");
-        }
-        if (userService.existsByEmail(userRecordDto.email())){
-            logger.warn("Username {} is already exists", userRecordDto.email());
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("esse Email ja existe");
+        userValidator.validate(userRecordDto, errors);
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors.getAllErrors());
         }
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(userRecordDto));
     }
+//        if (userService.existsByUsername(userRecordDto.username())){
+//            logger.warn("Username {} is already exists", userRecordDto.username());
+//            return ResponseEntity.status(HttpStatus.CONFLICT).body("esse Username ja existe");
+//        }
+//        if (userService.existsByEmail(userRecordDto.email())){
+//            logger.warn("Username {} is already exists", userRecordDto.email());
+//            return ResponseEntity.status(HttpStatus.CONFLICT).body("esse Email ja existe");
+//        }
+
 
     @GetMapping("/logs")
     public String index(){
